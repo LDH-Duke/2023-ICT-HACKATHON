bde68d7d38a8e32d9917b1e150f07d231aa851d1
Donghee Lee <ehdgml506@naver.com>
2023-08-06 17:18:46 +0900

    fea : db 연동중

--- backend/server.py
+++ backend/server.py
@@ -1,13 +1,31 @@
 from flask import Flask
+import pymysql
 
 app = Flask(__name__)
+
+# Mysql 연결 정보
+
+
+def connect_db():
+    return pymysql.connect(
+        user='root',
+        passwd='123456',
+        host='localhost',
+        db='mydb',
+        charset='utf8',
+    )
 
 
 @app.route('/main')
 def main():
-    return {"members": [{"id": 1, "name": "Duke"},
-                        {"id": 2, "name": "mem2"},
-                        {"id": 3, "name": "mem3"}]}
+    try:
+        # db연결
+        connection = connect_db()
+
+        return 0
+
+    except Exception as e:
+        return print('main error')
 
 
 if __name__ == "__main__":
